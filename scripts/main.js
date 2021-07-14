e238de299e755d2d1b56262bfb06a7375cc12b71
Maximiliano Esteban <maxesteban@hotmail.com>
2021-07-14 16:22:44 -0300

    Logica Menu Hamburguesa desde Master repaso 3

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,5 @@
 // Reglas Generales
+menuBtn = document.querySelector('nav .btn')
+menuLst = document.querySelector('nav .menu')
+
+menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
