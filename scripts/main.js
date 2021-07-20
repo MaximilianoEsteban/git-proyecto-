379c2c1619429554ea86e78a3110dbb049f431b2
MaximilianoEsteban <87046227+MaximilianoEsteban@users.noreply.github.com>
2021-07-20 13:11:45 -0300

    Delete main.js

--- scripts/main.js
+++
@@ -1,5 +0,0 @@
-//Navigation
-    menuBtn = document.querySelector('nav .btn')
-    menuLst = document.querySelector('nav .menu')
-
-    menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
